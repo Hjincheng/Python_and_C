ef019ccd1dcac1626ed30697f476af992d751a00
jincheng <service@t-firefly.com>
2019-08-25 17:06:35 +0800

    init Python and C++ repository

---
+++ python/hello.py
@@ -0,0 +1,7 @@
+a = 1
+b = 2
+c = 0
+print(a or b and c)
+
+if a :
+	print("ok")
---
+++ python/if_code.py
@@ -0,0 +1,15 @@
+account = 'xiaoming'
+password = '123456'
+
+
+print('please input account name:')
+user_name = input()
+
+print('please input password')
+user_password = input()
+
+if account == user_name and password == user_password :
+	print('success')
+else :
+	print('fail')
+
